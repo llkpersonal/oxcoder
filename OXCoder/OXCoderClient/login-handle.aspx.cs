58d778c8fcbb293a6697d54319e8b08fb2fe21bc
Lingkun <llkpersonal@126.com>
2015-06-21 17:29:45 +0800

    首页和登录

---
+++ OXCoder/OXCoderClient/login-handle.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OXCoderClient
+{
+    public partial class login_handle : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write(Request.Params["email"]);
+            Response.Write(Request.Params["password"]);
+            Response.Redirect("index.aspx");
+        }
+    }
+}
