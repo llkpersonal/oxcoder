de37cedc04d841107dab252188c2ffe637392c33
Tracy <lpxiang@bjtu.edu.cn>
2015-06-25 01:04:33 +0800

    csgsdhfhd

---
+++ OXCoder/IBLL/IChallegeService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OXCoder.IBLL
+{
+    interface IChallegeService
+    {
+
+    }
+}
---
+++ OXCoder/OXCoderClient/user-recruit-list.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OXCoderClient
+{
+    public partial class user_recruit_list : System.Web.UI.Page
+    {
+
+        protected string uname = string.Empty;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (null != Session && null != Session["uid"])
+            {
+                uname = (String)Session["name"];
+
+            }
+            else
+            {
+                Response.Redirect("login.aspx");
+            }
+        }
+    }
+}
